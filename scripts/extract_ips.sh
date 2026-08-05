17d744b944d558ab0c0e21e18feddcc70ea7551f
Yash Joshi <joshiyj08@gmail.com>
2026-08-05 15:23:26 +0530

    Fix IP extraction

--- scripts/extract_ips.sh
+++ scripts/extract_ips.sh
@@ -9,7 +9,5 @@ docker logs nginx 2>&1 | tail -20
 
 echo ""
 echo "===== Unique IPs ====="
-docker logs nginx 2>&1 \
-| awk '{print $1}' \
-| grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' \
-| sort -u
+
+docker logs nginx 2>&1 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u
